648c8d7bab2c5981242a4a254eb79dee6484c28d
Raditya Tresna Dwimurti <raditiya.tresna.dwimurti18@gmail.com>
2026-06-23 14:54:34 +0700

    perbaikan

--- main.go
+++ main.go
@@ -2,34 +2,42 @@ package main
 
 import (
 	"log"
+
 	"os"
 
 	"project-dimsum/config"
+
 	"project-dimsum/routes"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// 1. Ambil kunci Firebase dari Environment Variable (untuk di Render)
-	firebaseKeyEnv := os.Getenv("FIREBASE_KEY")
 
-	// 2. Nyalakan Database (Mengirimkan string env jika ada)
-	// Catatan: Jika fungsi ConnectFirebase() kamu belum menerima parameter,
-	// kamu bisa memodifikasi fungsi tersebut di file config/firebase.go
-	config.ConnectFirebase(firebaseKeyEnv)
+	// 1. Nyalakan Database
+
+	config.ConnectFirebase()
+
 	defer config.DB.Close()
 
-	// 3. Siapkan Router
+	// 2. Siapkan Router
+
 	r := gin.Default()
+
 	routes.SetupRoutes(r)
 
-	// 4. Jalankan Server
+	// 3. Jalankan Server
+
 	port := os.Getenv("PORT")
+
 	if port == "" {
-		port = "8080" // Render akan otomatis menyediakan port sendiri lewat env "PORT"
+
+		port = "8080"
+
 	}
 
 	log.Printf("Server Dimsum berjalan di port %s", port)
+
 	r.Run(":" + port)
+
 }
