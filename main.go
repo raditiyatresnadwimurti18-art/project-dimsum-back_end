8f2930b099edbff812e62c83f5cebd9c87237f56
Raditya Tresna Dwimurti <raditiya.tresna.dwimurti18@gmail.com>
2026-06-23 15:42:57 +0700

    perbaikan izin

--- main.go
+++ main.go
@@ -23,7 +23,18 @@ func main() {
 	// 2. Siapkan Router
 
 	r := gin.Default()
-
+	// Tambahkan Middleware CORS ini agar Frontend Web (CORS) diizinkan mengambil data
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
 	routes.SetupRoutes(r)
 
 	// 3. Jalankan Server
